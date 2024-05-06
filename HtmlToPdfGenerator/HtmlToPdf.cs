830e1893bc0111b8124c9ef47bfb8932a29eb222
Amisha <amishagupta2587@gmail.com>
2024-05-06 12:44:44 +0530

    Add project files.

---
+++ HtmlToPdfGenerator/HtmlToPdf.cs
@@ -0,0 +1,73 @@
+﻿using DinkToPdf;
+using DinkToPdf.Contracts;
+using HtmlToPdfGenerator;
+
+
+public class HtmlToPdf : IHtmlToPdfGenerator
+{
+    private readonly IConverter _converter;
+
+    public HtmlToPdf(IConverter converter)
+    {
+        _converter = converter;
+    }
+    /// <summary>
+    /// method that converts html content into pdf format
+    /// </summary>
+    /// <param name="htmlContent"></param>
+    /// <param name="paginationEnabled"></param>
+    /// <param name="documentTitle"></param>
+    /// <returns></returns>
+
+    public byte[] ConvertHtmlToPdf(string htmlContent, bool paginationEnabled = true, string? documentTitle = null)
+    {
+        htmlContent= $"<h1>{documentTitle}</h1>{htmlContent}";        
+
+        var doc = new HtmlToPdfDocument()
+        {
+            GlobalSettings =
+            {
+                PaperSize = PaperKind.A4,
+                Orientation = Orientation.Portrait                
+            }        
+            
+        };
+        var objectSettings = new ObjectSettings
+        {
+            HtmlContent = htmlContent,
+
+            WebSettings = new WebSettings
+            {
+                DefaultEncoding = "utf-8"
+            },
+
+            // Configure header settings
+
+            HeaderSettings = new HeaderSettings
+            {
+                FontName = "Calibri",
+                FontSize = 9,
+                Line = true,
+                Center = "Cognine"
+            },
+
+            // Configure footer settings
+            FooterSettings = new FooterSettings
+            {
+                FontName = "Calibri",
+                FontSize = 9,
+                Line = true,
+                Center = "Page [page] of [toPage]"
+            }
+
+        };
+        // Add the object settings to the document
+        doc.Objects.Add(objectSettings);
+        var pdfBytes = _converter.Convert(doc);
+        return pdfBytes;
+
+        
+    }  
+   
+    
+}
---
+++ HtmlToPdfGenerator/IHtmlToPdfGenerator.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace HtmlToPdfGenerator{
+    
+        public interface IHtmlToPdfGenerator
+        {
+            /// <summary>
+            /// Converts HTML content to a PDF file with pagination.
+            /// </summary>
+            /// <param name="htmlContent">The HTML content to convert.</param>
+            /// <param name="includePagination">Indicates whether to include pagination in the PDF.</param>
+            /// <returns>A byte array representing the generated PDF file.</returns>
+            byte[] ConvertHtmlToPdf(string htmlContent,bool includePagination,string documentTitle);
+        }
+    
+}
